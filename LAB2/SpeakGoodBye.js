2e96c027695d38f60e3f192f76db0d53a91b65f1
Olexsandrik <119703838+Olexsandrik@users.noreply.github.com>
2024-04-02 00:01:35 +0300

    Create SpeakGoodBye.js

---
+++ LAB2/SpeakGoodBye.js
@@ -0,0 +1,14 @@
+(function(window) {
+ var speakWord = "Good Bye";
+ var speakGoodBye = {};
+
+
+ speakGoodBye.speak = function(name) {
+   console.log(speakWord + " " + name);
+ };
+
+
+ window.speakGoodBye = speakGoodBye;
+
+
+})(window);
