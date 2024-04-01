a68cef228a0c6de6d04f1bb733ec6ef90fd388a7
Olexsandrik <119703838+Olexsandrik@users.noreply.github.com>
2024-04-02 00:00:54 +0300

    Create SpeakHello.js

---
+++ LAB2/SpeakHello.js
@@ -0,0 +1,14 @@
+(function(window) {
+ var speakWord = "Hello";
+ var speakHello = {};
+
+
+ speakHello.speak = function(name) {
+   console.log(speakWord + " " + name);
+ };
+
+
+ window.speakHello = speakHello;
+
+
+})(window);
