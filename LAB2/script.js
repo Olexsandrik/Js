c2a9321eb325805baaab9f8519789bdbefa64f79
Olexsandrik <119703838+Olexsandrik@users.noreply.github.com>
2024-04-24 19:25:41 +0300

    Update script.js

--- LAB2/script.js
+++ LAB2/script.js
@@ -1,16 +1,28 @@
 (function() {
- var names = ["John", "Jane", "Jack", "Jill", "James", "Alice", "Bob"];
+  var names = ["John", "Jane", "Jack", "Jill", "James", "Alice", "Bob"];
 
+  console.log("Перший варіант");
+  for (var i = 0; i < names.length; i++) {
+      var name = names[i];
+      var firstLetter = name.charAt(0).toLowerCase();
 
- for (var i = 0; i < names.length; i++) {
-     var name = names[i];
-     var firstLetter = name.charAt(0).toLowerCase();
-
-
-     if (firstLetter === "j") {
-         speakHello.speak(name);
-     } else {
-         speakGoodBye.speak(name);
-     }
- }
+      if (firstLetter === "j") {
+          speakHello.speak(name);
+      } else {
+          speakGoodBye.speak(name);
+      }
+  }
+  console.log("мій варінт")
+  for(var j=0; j<names.length; j++){
+    var name=names[j];
+    var lastLetter =name.charAt(name.length - 1);
+    if(lastLetter==="e"){
+        speakHello.speak(name);
+    }
+    else{
+        speakGoodBye.speak(name)
+    }
+  }
 })();
+
+
